cbb177ffd62ca4d84b57b350cdee759782fa8c91
tientv <tienco201@gmai.com>
2025-01-13 17:53:31 +0700

    feat: discord UI

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,7 +1,10 @@
+import { DiscordMessage } from "@/components/discord-message"
 import { Heading } from "@/components/heading"
 import { MaxWidthWrapper } from "@/components/max-width-wrapper"
-import { Check } from "lucide-react"
+import { MockDiscordIU } from "@/components/mock-discord-ui"
 import { ShinyButton } from "@/components/shinny-button"
+import { AnimatedList } from "@/components/ui/animated-list"
+import { Check } from "lucide-react"
 
 const Page = () => {
   return (
@@ -48,6 +51,59 @@ const Page = () => {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <MockDiscordIU>
+                <AnimatedList>
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="Signup"
+                    badgeColor="#43b581"
+                    title="👤 New user signed up"
+                    content={{
+                      name: "TienTV",
+                      email: "tienco201@gmail.com",
+                    }}
+                  />
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="Revenue"
+                    badgeColor="#faa61a"
+                    title="💰 Payment received"
+                    content={{
+                      amount: "$49.00",
+                      email: "tientv@newwave.com.vn",
+                      plan: "PRO",
+                    }}
+                  />
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda"
+                    username="PingPanda"
+                    timestamp="Today at 5:11AM"
+                    badgeText="Milestone"
+                    badgeColor="#5865f2"
+                    title="🎉 Revenue Milestone Achieved"
+                    content={{
+                      recurringRevenue: "$5.000 USD",
+                      growth: "+8.2%",
+                    }}
+                  />
+                </AnimatedList>
+              </MockDiscordIU>
+            </div>
+          </MaxWidthWrapper>
+        </div>
       </section>
     </>
   )
