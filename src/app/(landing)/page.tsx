bb765cf3d53977199804ae5c5ae4bdeb49d47c4b
tientv <tienco201@gmai.com>
2025-01-14 17:54:41 +0700

    style part - bento grid componet

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,6 +5,7 @@ import { MockDiscordIU } from "@/components/mock-discord-ui"
 import { ShinyButton } from "@/components/shinny-button"
 import { AnimatedList } from "@/components/ui/animated-list"
 import { Check } from "lucide-react"
+import Image from "next/image"
 
 const Page = () => {
   return (
@@ -52,6 +53,7 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
+      {/* discord ui */}
       <section className="relative bg-brand-25 pb-4">
         <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
         <div className="relative mx-auto">
@@ -104,6 +106,70 @@ const Page = () => {
             </div>
           </MaxWidthWrapper>
         </div>
+      </section>
+
+      {/* bento grid */}
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+          <div>
+            <h2 className="text-center text-base/7 font-semibold">
+              Intuitive Monitoring
+            </h2>
+            <Heading>Stay ahead with real-time insights</Heading>
+          </div>
+          <div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+            {/* first bento grid element */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Real-time notifications
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Get notified about critiacl evens the moment they happen, no
+                    matter if you&apos;re at home or on the go
+                  </p>
+                </div>
+                <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                  <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                    <Image
+                      className="size-full object-cover object-top"
+                      src="/phone-screen.png"
+                      alt="Phone screen displaying app interface"
+                      fill
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
+            </div>
+            {/* second bento grid element */}
+            <div className="relative max-lg:row-start-1">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-[calc(2rem+1px)]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Track Any Event
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    From new user signups to successful payments, PingPanda
+                    notifies you for all critical events in your Saas.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2 ">
+                  <Image
+                    src="/bento-any-event.png"
+                    alt="Bento box illustrating event tracking"
+                    width={500}
+                    height={300}
+                    className="w-full max-lg:max-w-xs"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
       </section>
     </>
   )
